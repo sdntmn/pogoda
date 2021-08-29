a814220601ad624b64ca27c9220e5af43efb54b0
sdntmn <67434735+sdntmn@users.noreply.github.com>
2021-08-29 20:27:08 +0500

    subject
    Погодный информер на реакте

---
+++ src/components/Weather.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Weather = (props) => {
+  console.log(props)
+  return (
+    <div>
+      { props.city &&
+        <div>
+          <p>Местоположение: {props.city}, {props.country}</p>
+          <p>Температура: {props.temp} °C</p>
+          <p>Скорость ветра: {props.wind} м.сек</p>
+          <p>Давление: {props.pressure} мм рт.ст.</p>
+          <p>Заход солнца: {props.sunset}</p>
+        </div>
+      }
+      <p>{props.error}</p>
+      </div>
+  );
+}
+
+export default Weather;
